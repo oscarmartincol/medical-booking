1fd10c99f21b4fc64cd5b9c3afb80f9053dc088b
Oscar Martinez <oscarmartincol@outlook.com>
2025-06-28 18:21:09 -0500

    Creación del proyecto.

---
+++ src/main/java/com/oscarmartincol/med_booking_api/MedBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.oscarmartincol.med_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedBookingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oscarmartincol/med_booking_api/MedBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarmartincol.med_booking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
